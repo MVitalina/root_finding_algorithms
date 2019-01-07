29b88c35694f4c03a90faf7d291eb527e0669863
Мірошник Віталіна <reckless.vita@gmail.com>
2019-01-08 01:38:33 +0200

    identify funcs

--- src/Func.java
+++ src/Func.java
@@ -1,8 +1,7 @@
 import static java.lang.Math.*;
 
 public abstract class Func {
-    public Func() {
-    }
+    public Func() {}
 
     public abstract double f(double x);
 
@@ -17,18 +16,22 @@ class Func43 extends Func{
 
     @Override
     public double f(double x){
+        return ( pow((5-x), 0.5) * sin(x) + x * cos(PI - x) );
     }
 
     @Override
     public double der1(double x){
+        return ( sin(x) * (x - 1/(2*pow((5-x), 0.5))) + cos(x) * (pow((5-x), 0.5) - 1) );
     }
 
     @Override
     public double der2(double x){
+        return ( sin(x) * (2 - 1/(4*pow((5-x), 1.5)) - pow((5-x), 0.5)) + cos(x) * (x - 1/pow((5-x), 0.5)) );
     }
 
     @Override
     public double fX(double x){
+        return 0;
     }
 }
 
@@ -36,17 +39,21 @@ class Func47 extends Func{
 
     @Override
     public double f(double x){
+        return (x * cos(cosh(x - PI)) + 0.3 * x);
     }
 
     @Override
     public double der1(double x){
+        return (cos(cosh(x - PI)) - x * sinh(x-PI) * sin(cosh(x - PI)) + 0.3);
     }
 
     @Override
     public double der2(double x){
+        return (-x * cosh(x-PI) * sin(cosh(x - PI)) - sinh(x-PI)*(2*sin(cosh(x-PI)) + x * sinh(x-PI)*cos(cosh(x-PI))));
     }
 
     @Override
     public double fX(double x){
+        return 0;
     }
 }
